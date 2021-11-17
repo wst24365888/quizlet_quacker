ed2d975bf1ed5de7f9a2ac0ccca37b15e16915ec
Xyphuz <hansen0715.2@gmail.com>
2021-11-17 23:36:28 +0800

    style: modify button appearance

--- src/ts/popup.ts
+++ src/ts/popup.ts
@@ -1,7 +1,7 @@
 import { QuizletQuackerSwitchController } from './quizlet_quacker_switch_controller.js';
 
 let qqsc = new QuizletQuackerSwitchController();
-let qqs: HTMLInputElement | null = <HTMLInputElement | null>document.getElementById("qqs");
+let qqs: HTMLInputElement | null = <HTMLInputElement | null>document.getElementById("qq-switch");
 
 if (qqs != null) {
     qqs.addEventListener('click', () => {
